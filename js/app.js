68c3269556db9a280f7c691e22e75f5e411687e2
Rose Kairu <rosekairu@gmail.com>
2020-04-22 16:25:29 +0300

    modify script file

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ License: Creative Commons Attribution 4.0 Unported
 License URL: https://creativecommons.org/licenses/by/4.0/
 */
 
-(function($) {
+(function ($) {
 
     "use strict"; // Start of use strict
-    
+
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
@@ -22,7 +22,7 @@ License URL: https://creativecommons.org/licenses/by/4.0/
     });
 
     // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
+    $('.js-scroll-trigger').click(function () {
         $('.navbar-collapse').collapse('hide');
     });
 
@@ -33,7 +33,7 @@ License URL: https://creativecommons.org/licenses/by/4.0/
     });
 
     //fixed navbar
-    var toggleAffix = function(affixElement, scrollElement, wrapper) {
+    var toggleAffix = function (affixElement, scrollElement, wrapper) {
 
         var height = affixElement.outerHeight(),
             top = wrapper.offset().top;
@@ -48,12 +48,12 @@ License URL: https://creativecommons.org/licenses/by/4.0/
 
     };
 
-    $('[data-toggle="affix"]').each(function() {
+    $('[data-toggle="affix"]').each(function () {
         var ele = $(this),
             wrapper = $('<div></div>');
 
         ele.before(wrapper);
-        $(window).on('scroll resize', function() {
+        $(window).on('scroll resize', function () {
             toggleAffix(ele, $(this), wrapper);
         });
 
@@ -67,7 +67,7 @@ License URL: https://creativecommons.org/licenses/by/4.0/
 /*====================================================
                     TESTIMONIALS
 ====================================================*/
-$(function() {
+$(function () {
     $("#testimonial-slider").owlCarousel({
         items: 2,
         smartSpeed: 700,
@@ -92,37 +92,93 @@ $(function() {
     });
 });
 
+$(document).ready(function () {
+
+    /* 1. Visualizing things on Hover - See next part for action on click */
+    $('#stars li').on('mouseover', function () {
+        var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+
+        // Now highlight all the stars that's not after the current hovered star
+        $(this).parent().children('li.star').each(function (e) {
+            if (e < onStar) {
+                $(this).addClass('hover');
+            } else {
+                $(this).removeClass('hover');
+            }
+        });
+
+    }).on('mouseout', function () {
+        $(this).parent().children('li.star').each(function (e) {
+            $(this).removeClass('hover');
+        });
+    });
+
+
+    /* 2. Action to perform on click */
+    $('#stars li').on('click', function () {
+        var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+        var stars = $(this).parent().children('li.star');
+
+        for (i = 0; i < stars.length; i++) {
+            $(stars[i]).removeClass('selected');
+        }
+
+        for (i = 0; i < onStar; i++) {
+            $(stars[i]).addClass('selected');
+        }
+
+        // JUST RESPONSE (Not needed)
+        var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+        var msg = "";
+        if (ratingValue > 1) {
+            msg = "Thank You! You rated this " + ratingValue + " stars.";
+        } else {
+            msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
+        }
+        responseMessage(msg);
+
+    });
+
+
+});
+
+
+function responseMessage(msg) {
+    $('.success-box').fadeIn(200);
+    $('.success-box div.text-message').html("<span>" + msg + "</span>");
+}
+
 /*====================================================
                         PORTFOLIO
 ====================================================*/
-  $(function() {
+$(function () {
 
-      $("#portfolio").magnificPopup({
-          delegate: 'a', // child items selector, by clicking on it popup will open
-          type: 'image',
-          gallery: {
-              enabled: true
-          }
+    $("#portfolio").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
 
-      });
-  });
+    });
+});
 
 /* ====================================================
                        BACK TO TOP
 =======================================================*/
-  (function($) {
+(function ($) {
 
-      $(window).scroll(function() {
+    $(window).scroll(function () {
 
-          if ($(this).scrollTop() < 50) {
-              // hide nav
-              $("nav").removeClass("ss-top-nav");
-              $("#back-to-top").fadeOut();
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            $("nav").removeClass("ss-top-nav");
+            $("#back-to-top").fadeOut();
 
-          } else {
-              // show nav
-              $("nav").addClass("ss-top-nav");
-              $("#back-to-top").fadeIn();
-          }
-      });
-  })(jQuery); // End of use strict
+        } else {
+            // show nav
+            $("nav").addClass("ss-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+})(jQuery); // End of use strict
